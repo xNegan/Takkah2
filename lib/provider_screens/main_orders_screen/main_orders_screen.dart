867f72781ed593feeeb7983378f337700c7a3477
khilad <kmtr467@gmail.com>
2022-09-05 15:06:03 +0300

    add diloag

--- lib/provider_screens/main_orders_screen/main_orders_screen.dart
+++ lib/provider_screens/main_orders_screen/main_orders_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -382,6 +385,52 @@ class _MainOrdersScreenState extends State<MainOrdersScreen> {
                 InkWell(
                   onTap: () {
                     //Dialog
+                    if (Platform.isAndroid) {
+                      dialog(onTap: (){
+                        Get.back();
+                        dialogSucsess();
+                      });
+                    } else {
+                      Get.dialog(CupertinoAlertDialog(
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: new BoxDecoration(
+                                  //  color: Colors.green,
+                                    borderRadius: new BorderRadius.all(Radius.circular(50))),
+                                child: Icon(
+                                  Icons.clear,
+                                  size: 80,
+                                  color: AppColors.redColor,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.h,
+                              ),
+                              TextCustom(
+                                text: "Title",
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                // color: AppColors.mainYellow,
+                              ),
+                              SizedBox(
+                                height: 10.h,
+                              ),
+                            ],
+                          ),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: TextCustom(
+                                  text: 'Ok',
+                                  color: AppColors.redColor,
+                                ))
+                          ]));
+                    }
                   },
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
@@ -421,7 +470,124 @@ class _MainOrdersScreenState extends State<MainOrdersScreen> {
       ),
     );
   }
+  Future<dynamic> dialog({required Function() onTap,
+   // required TextEditingController controller,
+  }) {
+    return Get.dialog(
+                      AlertDialog(
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextCustom(
+                              text: "إضافة وقت التجهيز",
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              // color: AppColors.mainYellow,
+                            ),
+                            SizedBox(
+                              height: 40.h,
+                            ),
+                            Container(
+                              margin: EdgeInsetsDirectional.only(start: 80.w,end: 80.w),
+                              height: 85.h,
+                              decoration: new BoxDecoration(
+                                  borderRadius: new BorderRadius.all(Radius.circular(10)),
+                              border: Border.all(color: AppColors.maincolor)
+                              ),
+                              child:  TextFormField(
+                              //  controller: controller,
+                                keyboardType: TextInputType.datetime,
+                                toolbarOptions: const ToolbarOptions(
+                                  copy: true,
+                                  cut: true,
+                                  paste: true,
+                                  selectAll: true,
+                                ),
+                                style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 14.sp,
+                                  color: AppColors.blackColor,
+                                ),
+                                decoration: InputDecoration(
+                                  isDense: true,
+                                  fillColor: Colors.transparent,
+                                  filled: true,
+                                  hintText: 'ادخل الوقت',
+                                  hintStyle: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 13.sp,
+                                    color: AppColors.lGray,
+                                  ),
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 16.w, vertical: 12.h),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(24.r),
+                                    borderSide: BorderSide.none,
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(24.r),
+                                    borderSide: BorderSide.none,
+                                  ),
+                                  errorBorder: InputBorder.none,
+                                ),
+                              ),
+                            ),
 
+                            SizedBox(
+                              height: 30.h,
+                            ),
+                            InkWell(
+                              onTap: onTap,
+                              child:  Container(
+                                margin: EdgeInsetsDirectional.only(start: 50.w,end: 50.w),
+                                height: 50.h,
+                                decoration: new BoxDecoration(
+                                    color: AppColors.maincolor,
+                                    borderRadius:
+                                    new BorderRadius.all(Radius.circular(10))),
+                                child: Center(
+                                  child: TextCustom(
+                                    text: "تأكيد",
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                      ),
+                    );
+  }
+  Future<dynamic> dialogSucsess() {
+    return Get.dialog(
+      AlertDialog(
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/check.png',
+              width: 100.w,
+            ),
+            SizedBox(height: 50.h,),
+            TextCustom(
+              text: "تم إضافة الوقت بنجاح",
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              // color: AppColors.mainYellow,
+            ),
+          ],
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30.0))),
+      ),
+    );
+  }
   AppBar _appBar() {
     return AppBar(
       bottom: _tabBar(),
